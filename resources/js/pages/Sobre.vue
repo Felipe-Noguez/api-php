71cc5481a9d9beaccde21fe7a5398d107d931051
Felipe-Noguez <felipenoguez@outlook.com.br>
2025-07-18 11:49:24 -0300

    feat: adicionado pagina sobre

---
+++ resources/js/pages/Sobre.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="p-6">
+    <h1 class="text-2xl font-bold">Sobre 1</h1>
+    <p class="mt-4">Esse é um exemplo básico usando Inertia + Vue.</p>
+  </div>
+</template>
+
+<!-- <script setup> -->
+<!-- // Nada por enquanto, apenas conteúdo estático. -->
+<!-- </script> -->
