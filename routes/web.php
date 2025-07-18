88798c57d9c7fae5b5e471359c758dca4c431f54
Felipe-Noguez <felipenoguez@outlook.com.br>
2025-07-18 11:51:54 -0300

    feat: adicionado rotas contato e sobre

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\SiteController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome');
 })->name('home');
+
+Route::get('/sobre', function () {
+    return Inertia::render('Sobre');
+})->name('sobre');
+
+Route::get('/contato', [SiteController::class, 'contato'])->name('contato');
+
+// Route::get('/contato', function () {
+//     return '<h1>Página de Contato</h1><p>Esta é a sua primeira rota de contato!</p>';
+// });
 
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
